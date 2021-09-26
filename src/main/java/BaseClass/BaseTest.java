ac65d91473547f85bf6165d6df9762d9a09a1289
SachinGokul <sachinrules166@gmail.com>
2021-09-26 12:55:11 +0530

    BaseTest is used to setup the before/after test actions

---
+++ src/main/java/BaseClass/BaseTest.java
@@ -0,0 +1,29 @@
+package BaseClass;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import GenericFunctions.GenericFunctions;
+import Utilities.CommonStrings;
+import Utilities.PropertyReader;
+
+public class BaseTest {
+
+	public static WebDriver driver;
+	
+	//startTest method used to initate the driver instance
+	@BeforeClass
+	public void startTest() {
+		driver = GenericFunctions.initiatebrowser(PropertyReader.getProperty("Browser"));
+
+	}
+	
+	//tearDown method to terminate the browser instance
+	@AfterClass
+	public void tearDown()
+	{
+		
+	}
+
+}
